f7de603214b03a863dcf231194b43a8b7cf661b6
AkankshaS-Code <53638027+AkankshaS-Code@users.noreply.github.com>
2020-05-28 14:19:25 +0530

    Update app_a.py

--- app_a.py
+++ app_a.py
@@ -1,23 +1,9 @@
-from flask import Flask, request
-import requests
-application = Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
-
-@application.route('/hello')
+@app.route("/")
 def hello():
-    return 'Hello there'
-
-
-@application.route('/jobs', methods=['POST'])
-def jobs():
-    token = request.headers['Authorization']
-    data = {"token": token}
-    result = requests.post('http://0.0.0.0:5001/auth', data=data).content
-    if result == "density":
-        return 'Jobs:\nTitle: Devops\nDescription: Awesome\n'
-    else:
-        return 'fail'
-
+    return "Hello World!"
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=5000)
+    app.run(ssl_context='adhoc')
